d452a18c06177019ff8ac55deec1fb9dafbb90e3
bsandusky2 <bsandus@ncsu.edu>
2023-06-10 15:51:50 -0400

    generate fucntions

--- R/shinyFunctions.R
+++ R/shinyFunctions.R
@@ -1,21 +1,48 @@
 
-usaSharkAttacks<- function(attackDF, type = "fatal"){
+usaSharkAttacks<- function(attackDF, type = "fatal", group){
   #Filter down to just the states
+  browser()
   attackDF<- attackDF %>% filter(
-    !Area %in% c("Puerto Rico", "US Virgin Islands", "Midway Atoll", "CUBA", "East cost",
+    country == "USA",
+    !{{group}}[1] %in% c("Puerto Rico", "US Virgin Islands", "Midway Atoll", "CUBA", "East cost",
                  "Johnston Atoll", "Palmyra Atoll", "Wake Island" , "Guam")
   )
+  #Create attackFatal column
+  if(type == "fatal"){
+    attackDF2<- attackDF %>% 
+      mutate(attackFatal = case_when(grepl("y", fatal_y_n, ignore.case = T) ~ 1, TRUE ~ 0))
+  } else{
+    attackDF2<- attackDF %>% 
+      mutate(attackFatal = 1)
+  }
+  
+  attachDF2<- attackDF2 %>% 
+    group_by(!!sym(group[1]), !!sym(group[2])) %>% summarise(total = sum(attackFatal))
+  
+  return(attachDF2)
+  
 }
 
+usaAttack<- usaSharkAttacks(attacks, group = c("area"))
 
-testSummary<- test %>% 
-  mutate(fatal = case_when(grepl("y", `Fatal (Y/N)`, ignore.case = T) ~ 1, TRUE ~ 0)) %>% 
-  group_by(Area) %>% summarise(totalFatal = sum(fatal)) %>% 
-  rename(state = Area)
-
-plot_usmap(data = testSummary, values = 'totalFatal', regions = "states")  + 
-  labs(title = "United States Attacks/Fatalities", subtitle = "Poverty Percentage Estimates for New England Counties in 2014") +
+#USA Map
+plot_usmap(data = usaAttack, values = 'total', regions = "states")  + 
+  labs(title = "United States Attacks/Fatalities") +
   theme(legend.position = "right")
+
+#Area Chart
+fig <- plot_ly(usaAttack, x = ~year, y = ~Food.and.Tobacco, name = 'Food and Tobacco', type = 'scatter', mode = 'none', stackgroup = 'one', fillcolor = '#F5FF8D')
+fig <- fig %>% add_trace(y = ~Household.Operation, name = 'Household Operation', fillcolor = '#50CB86')
+fig <- fig %>% add_trace(y = ~Medical.and.Health, name = 'Medical and Health', fillcolor = '#4C74C9')
+fig <- fig %>% add_trace(y = ~Personal.Care, name = 'Personal Care', fillcolor = '#700961')
+fig <- fig %>% add_trace(y = ~Private.Education, name = 'Private Education', fillcolor = '#312F44')
+fig <- fig %>% layout(title = 'United States Personal Expenditures by Categories',
+                      xaxis = list(title = "",
+                                   showgrid = FALSE),
+                      yaxis = list(title = "Expenditures (in billions of dollars)",
+                                   showgrid = FALSE))
+
+fig
 
 
 
--- global.R
+++ global.R
@@ -1,5 +1,15 @@
-packages<- c("shiny", "tidyverse", "plotly", "shinydashboard", "usmap")
+packages<- c("shiny", "tidyverse", "plotly", "shinydashboard", "usmap", "janitor")
 invisible(lapply(packages, library, character.only = T))
 source("./R/shinyFunctions.R")
 source("./Modules/metrics/metricsModule.R")
-attacks<- read_csv("attacks.csv")
+attacks<- readxl::read_xlsx("attacks.xlsx") %>% 
+  clean_names() %>% 
+  filter(!row_number() %in% c(2882)) %>% 
+  mutate(sharkType = case_when(
+    grepl("white", species, ignore.case = T) ~ "Great White Shark",
+    grepl("bull", species, ignore.case = T) ~ "Bull Shark",
+    grepl("tiger",species, ignore.case = T) ~ "Tiger Shark",
+    grepl("lemon", species, ignore.case = T) ~ "Lemon Shark",
+    grepl("hammer",species, ignore.case = T) ~ "Hammerhead Shark",
+    TRUE ~ "Other"
+  ))
